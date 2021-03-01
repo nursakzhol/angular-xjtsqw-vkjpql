75fa6aae6c0dab547a2609f740782b2032a1ef00
nursakzhol <nur260602@mail.ru>
2021-03-01 21:03:15 +0600

    3 files modified

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -14,6 +14,7 @@ export class ProductItemComponent {
     window.alert("The product has been shared!");
   }
 
+
   onNotify() {
     window.alert("You will be notified when the product goes on sale");
   }
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,98 +1,136 @@
 export const products = [
   {
     id: 1,
-    image: 'https://m.media-amazon.com/images/I/81AyGdH1VHL._AC_UL320_.jpg',
-    name: 'Amazon Basics Acrylic 6-Compartment Tall Durable Makeup Jewelry Accessories Storage Organizer Tray',
+    image: "https://m.media-amazon.com/images/I/81AyGdH1VHL._AC_UL320_.jpg",
+    name:
+      "Amazon Basics Acrylic 6-Compartment Tall Durable Makeup Jewelry Accessories Storage Organizer Tray",
     price: 16,
-    description: 'Clear acrylic storage organizer for accessories or makeup',
-    link: 'https://www.amazon.com/AmazonBasics-Accessory-Tray-6-Compartments/dp/B07FFTG66J/ref=sr_1_1_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQUxFUllZUUFKWjlQJmVuY3J5cHRlZElkPUEwNDYwNzE2M01KVFJMNUZDT0VGUiZlbmNyeXB0ZWRBZElkPUEwMDk2ODIyTDdISVExMDhBQUlJJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+    description: "Clear acrylic storage organizer for accessories or makeup",
+    link:
+      "https://www.amazon.com/AmazonBasics-Accessory-Tray-6-Compartments/dp/B07FFTG66J/ref=sr_1_1_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQUxFUllZUUFKWjlQJmVuY3J5cHRlZElkPUEwNDYwNzE2M01KVFJMNUZDT0VGUiZlbmNyeXB0ZWRBZElkPUEwMDk2ODIyTDdISVExMDhBQUlJJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==",
+    linkwp:
+      "https://wa.me/?text=https://www.amazon.com/AmazonBasics-Accessory-Tray-6-Compartments/dp/B07FFTG66J/ref=sr_1_1_sspa?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQUxFUllZUUFKWjlQJmVuY3J5cHRlZElkPUEwNDYwNzE2M01KVFJMNUZDT0VGUiZlbmNyeXB0ZWRBZElkPUEwMDk2ODIyTDdISVExMDhBQUlJJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
   },
   {
     id: 2,
-    image: 'https://m.media-amazon.com/images/I/71K7Q4FpguL._AC_UL320_.jpg',
-    name: 'Amazon Basics 8-Sheet Capacity, Cross-Cut Paper and Credit Card Shredder, 4.1 Gallon',
+    image: "https://m.media-amazon.com/images/I/71K7Q4FpguL._AC_UL320_.jpg",
+    name:
+      "Amazon Basics 8-Sheet Capacity, Cross-Cut Paper and Credit Card Shredder, 4.1 Gallon",
     price: 41,
-    description: 'Cross-cut shred size: turns paper into small confetti-like pieces measuring 3/16 by 1-27/32 inches (5 by 47 mm); meets security level P-3 standards',
-    link: 'https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/dp/B00YFTHJ9C/ref=sr_1_2?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-2'
+    description:
+      "Cross-cut shred size: turns paper into small confetti-like pieces measuring 3/16 by 1-27/32 inches (5 by 47 mm); meets security level P-3 standards",
+    link:
+      "https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/dp/B00YFTHJ9C/ref=sr_1_2?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-2",
+    linkwp:
+      "https://wa.me/?text=https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/dp/B00YFTHJ9C/ref=sr_1_2?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-2"
   },
   {
     id: 3,
-    image: 'https://m.media-amazon.com/images/I/71IdKRlm8+L._AC_UL320_.jpg',
-    name: 'Amazon Basics 48 Pack AA High-Performance Alkaline Batteries, 10-Year Shelf Life, Easy to Open Value Pack',
+    image: "https://m.media-amazon.com/images/I/71IdKRlm8+L._AC_UL320_.jpg",
+    name:
+      "Amazon Basics 48 Pack AA High-Performance Alkaline Batteries, 10-Year Shelf Life, Easy to Open Value Pack",
     price: 16,
-    description: 'AA 1.5-volt performance alkaline batteries for reliable performance across a wide range of devices',
-    link: 'https://www.amazon.com/AmazonBasics-Performance-Alkaline-Batteries-Count/dp/B00MNV8E0C/ref=sr_1_3?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-3'
+    description:
+      "AA 1.5-volt performance alkaline batteries for reliable performance across a wide range of devices",
+    link:
+      "https://www.amazon.com/AmazonBasics-Performance-Alkaline-Batteries-Count/dp/B00MNV8E0C/ref=sr_1_3?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-3",
+    linkwp:
+      "https://wa.me/?text=https://www.amazon.com/AmazonBasics-Performance-Alkaline-Batteries-Count/dp/B00MNV8E0C/ref=sr_1_3?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-3"
   },
 
   {
     id: 4,
-    image: 'https://m.media-amazon.com/images/I/71GUQhGm2aL._AC_UL320_.jpg',
-    name: 'Amazon Basics Microfiber Sheet Set, Twin, Dark Grey',
+    image: "https://m.media-amazon.com/images/I/71GUQhGm2aL._AC_UL320_.jpg",
+    name: "Amazon Basics Microfiber Sheet Set, Twin, Dark Grey",
     price: 15,
-    description: '100% Polyester Microfiber',
-    link: 'https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-5'
+    description: "100% Polyester Microfiber",
+    link:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-5",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-5"
   },
 
-   {
+  {
     id: 5,
-    image: 'https://m.media-amazon.com/images/I/71i08qnZeDL._AC_UL320_.jpg',
-    name: 'Amazon Basics Classic Puresoft PU-Padded Mid-Back Office Computer Desk Chair with Armrest - Black',
+    image: "https://m.media-amazon.com/images/I/71i08qnZeDL._AC_UL320_.jpg",
+    name:
+      "Amazon Basics Classic Puresoft PU-Padded Mid-Back Office Computer Desk Chair with Armrest - Black",
     price: 79,
-    description: 'Comfortable office chair upholstered in durable polyurethane bonded leather',
-    link: 'https://www.amazon.com/AmazonBasics-Puresoft-PU-Padded-Mid-Back-Computer/dp/B081H3Y5NW/ref=sr_1_6?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-6'
+    description:
+      "Comfortable office chair upholstered in durable polyurethane bonded leather",
+    link:
+      "https://www.amazon.com/AmazonBasics-Puresoft-PU-Padded-Mid-Back-Computer/dp/B081H3Y5NW/ref=sr_1_6?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-6",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-6"
   },
 
   {
     id: 6,
-    image: 'https://m.media-amazon.com/images/I/91qC+UYYwIL._AC_UL320_.jpg',
-    name: 'Amazon Basics Underseat Carry-On Rolling Travel Luggage Bag, 14 Inches, Grey',
+    image: "https://m.media-amazon.com/images/I/91qC+UYYwIL._AC_UL320_.jpg",
+    name:
+      "Amazon Basics Underseat Carry-On Rolling Travel Luggage Bag, 14 Inches, Grey",
     price: 63,
-    description: 'Grey, 14-Inch rolling luggage bag; made of durable 600D polyester; fits in most airline overhead compartments and under the seat in front of you',
-    link: 'https://www.amazon.com/AmazonBasics-A161214-5-Underseat-Luggage-Grey/dp/B07HM3F76L/ref=sr_1_7?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-7'
+    description:
+      "Grey, 14-Inch rolling luggage bag; made of durable 600D polyester; fits in most airline overhead compartments and under the seat in front of you",
+    link:
+      "https://www.amazon.com/AmazonBasics-A161214-5-Underseat-Luggage-Grey/dp/B07HM3F76L/ref=sr_1_7?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-7",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-7"
   },
 
   {
     id: 7,
-    image: 'https://m.media-amazon.com/images/I/710Bdk+7VfL._AC_UL320_.jpg',
+    image: "https://m.media-amazon.com/images/I/710Bdk+7VfL._AC_UL320_.jpg",
     name: 'Amazon Basics FP1904233 Frypan, 8" & 10", silver',
     price: 35,
-    description: '2-piece stainless steel fry pan set; 8 inch and 10 inch pans',
-    link: 'https://www.amazon.com/AmazonBasics-2-Piece-Stainless-10-Inch-8-Inch/dp/B07Y3KHWKQ/ref=sr_1_8?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-8'
+    description: "2-piece stainless steel fry pan set; 8 inch and 10 inch pans",
+    link:
+      "https://www.amazon.com/AmazonBasics-2-Piece-Stainless-10-Inch-8-Inch/dp/B07Y3KHWKQ/ref=sr_1_8?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-8",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-8"
   },
 
   {
     id: 8,
-    image: 'https://m.media-amazon.com/images/I/61stA5Z9W0L._AC_UL320_.jpg',
-    name: 'Amazon Basics Nylon Braided Lightning to USB Cable - MFi Certified Apple iPhone Charger, Dark Gray, 6-Foot (Durability Rated 4, 000 Bends) upgrade',
+    image: "https://m.media-amazon.com/images/I/61stA5Z9W0L._AC_UL320_.jpg",
+    name:
+      "Amazon Basics Nylon Braided Lightning to USB Cable - MFi Certified Apple iPhone Charger, Dark Gray, 6-Foot (Durability Rated 4, 000 Bends) upgrade",
     price: 13,
-    description: 'Universal Compatibility: Compatible with the iPhone SE (2nd Gen) / 11 / XS / XS Max / XR / X / 8 Plus / 8 / 7 Plus / 7 / 6s Plus / 6s / 6 Plus / 6 / 5s / 5c / 5 / iPad Pro / iPad Air / Air 2 / iPad mini / mini 2 / mini 4 / iPad 4th gen / iPod Touch 5th gen / iPod nano 7th gen and Beats Pill+',
-    link: 'https://www.amazon.com/AmazonBasics-Nylon-Braided-Lightning-Cable/dp/B082T6GVKJ/ref=sr_1_9?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-9'
+    description:
+      "Universal Compatibility: Compatible with the iPhone SE (2nd Gen) / 11 / XS / XS Max / XR / X / 8 Plus / 8 / 7 Plus / 7 / 6s Plus / 6s / 6 Plus / 6 / 5s / 5c / 5 / iPad Pro / iPad Air / Air 2 / iPad mini / mini 2 / mini 4 / iPad 4th gen / iPod Touch 5th gen / iPod nano 7th gen and Beats Pill+",
+    link:
+      "https://www.amazon.com/AmazonBasics-Nylon-Braided-Lightning-Cable/dp/B082T6GVKJ/ref=sr_1_9?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-9",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-9"
   },
 
   {
     id: 9,
-    image: 'https://m.media-amazon.com/images/I/716vdWfenAL._AC_UL320_.jpg',
-    name: 'Amazon Basics 5-Shelf Adjustable, Heavy Duty Storage Shelving Unit (350 lbs loading capacity per shelf), Steel Organizer Wire Rack, Black,(36L x 14W x 72H)',
+    image: "https://m.media-amazon.com/images/I/716vdWfenAL._AC_UL320_.jpg",
+    name:
+      "Amazon Basics 5-Shelf Adjustable, Heavy Duty Storage Shelving Unit (350 lbs loading capacity per shelf), Steel Organizer Wire Rack, Black,(36L x 14W x 72H)",
     price: 70,
-    description: '5-shelf shelving unit for your kitchen, office, garage, and more',
-    link: 'https://www.amazon.com/AmazonBasics-5-Shelf-Shelving-Unit-Black/dp/B018YLFJX4/ref=sr_1_10?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-10'
+    description:
+      "5-shelf shelving unit for your kitchen, office, garage, and more",
+    link:
+      "https://www.amazon.com/AmazonBasics-5-Shelf-Shelving-Unit-Black/dp/B018YLFJX4/ref=sr_1_10?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-10",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-10"
   },
 
   {
     id: 10,
-    image: 'https://m.media-amazon.com/images/I/81gQzwHg2YL._AC_UL320_.jpg',
-    name: 'AmazonBasics Legal/Wide Ruled 8-1/2 by 11-3/4 Legal Pad - White (50 Sheet Paper Pads, 12 pack)',
+    image: "https://m.media-amazon.com/images/I/81gQzwHg2YL._AC_UL320_.jpg",
+    name:
+      "AmazonBasics Legal/Wide Ruled 8-1/2 by 11-3/4 Legal Pad - White (50 Sheet Paper Pads, 12 pack)",
     price: 13,
-    description: '12-pack of 50-page white legal pads',
-    link: 'https://www.amazon.com/AmazonBasics-Legal-Wide-Ruled-11-3/dp/B00QSR9URI/ref=sr_1_11?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-11'
-  },
-
-  
-
-
-
+    description: "12-pack of 50-page white legal pads",
+    link:
+      "https://www.amazon.com/AmazonBasics-Legal-Wide-Ruled-11-3/dp/B00QSR9URI/ref=sr_1_11?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-11",
+    linkwp:
+      "https://www.amazon.com/AmazonBasics-Microfiber-Sheet-Set-Twin/dp/B00Q7OAKV2/ref=sr_1_5?dchild=1&keywords=amazonbasics&pd_rd_r=442b1d69-0b5e-4a8b-b45b-d522838a8d97&pd_rd_w=zCwUY&pd_rd_wg=hYQNz&pf_rd_p=9349ffb9-3aaa-476f-8532-6a4a5c3da3e7&pf_rd_r=04HJTMZSEASRMG5E2V5V&qid=1614501353&sr=8-11"
+  }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
